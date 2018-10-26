e70fff0f5f699b80ebecd05abe9e951d465ca6b2
SANJAY K SAI <shachsan@hotmail.com>
2018-10-26 16:18:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(speak)
   if speak == love
     return 'I LOVE YOU TOO PUMPKIN!'
   elsif speak == speak.upcase && speak!=love
-    return NO, NOT SINCE 1938!
+    return 'NO, NOT SINCE 1938!'
   else
     return 'HUH?! SPEAK UP, SONNY!'
   end 
